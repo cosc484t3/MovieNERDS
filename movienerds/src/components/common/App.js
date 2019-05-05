5d163b4b05aa261e077e342c4f2b44cd30f90926
Ayja <aweems3@students.towson.edu>
2019-05-05 18:00:57 -0400

    Removed AppRouter: * Changed initial component rendered to App.js in the index.js file * Deleted AppRouter.js * Added Switch and routes from AppRouter.js to App.js * Connected App.js to Redux store

--- movienerds/src/components/common/App.js
+++ movienerds/src/components/common/App.js
@@ -1,16 +1,38 @@
-import React, { Component } from 'react';
-import '../../layout/App.css';
-import {Header} from './Header'
-import '../../layout/App.css'
-export const MOVIE_NERDS_API_URL = 'http://localhost:5000'
+//React & Redux imports
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { connect } from 'react-redux';
 
-export class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-      </div>
-    );
-  }
+//Component imports
+import { Header } from './Header';
+import Homepage from '../homepage/Homepage'
+import { AllMovies } from '../all-movies/AllMovies'
+import { RecentMovies } from '../recent-movies/RecentMovies'
+import { MovieQuotes } from '../movie-quotes/MovieQuotes'
+import CharacterDetails from '../movie-forum/CharacterDetails'
+import Forum from '../movie-forum/Forum'
+
+//CSS file(s)
+import '../../layout/App.css'
+
+const App = () => {
+  return (
+      <Router>
+        <>
+          <div className="App">
+            <Header />
+          </div>
+          <Switch>
+              <Route exact path="/" component={Homepage} />
+              <Route path="/movie/:id" component={Forum} />
+              <Route path="/all-movies" component={AllMovies} />
+              <Route path="/recent-movies" component={RecentMovies} />
+              <Route path="/movie-quotes" component={MovieQuotes} />
+              <Route path="/character-details/:id" component={CharacterDetails} />
+          </Switch>
+        </>
+      </Router>
+  );
 }
 
+export default connect()(App)
--- movienerds/src/index.js
+++ movienerds/src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux';
+// import logger from 'redux-logger'
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension'
+import rootReducer from './reducer';
 import './index.css';
-import AppRouter from './components/common/AppRouter'
+import App from './components/common/App'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<AppRouter />, document.getElementById('root'));
+export const MOVIE_NERDS_API_URL = 'http://localhost:5000'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+// const middleware = [logger, thunk]
+const middleware = [thunk]
+const store = createStore(
+  rootReducer, 
+  {}, 
+  composeWithDevTools(applyMiddleware(...middleware)))
+
+render(
+    <Provider store={store}>
+        <App /> 
+    </Provider>,
+    document.getElementById('root')
+);
+
 serviceWorker.unregister();
+
