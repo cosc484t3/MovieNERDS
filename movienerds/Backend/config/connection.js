19ae4dacccb3031b9472030ae98a6ca46f0dc63c
Bartholomew Allen <37280445+Ballen2713@users.noreply.github.com>
2019-06-05 14:38:51 -0400

    Delete connection.js

--- movienerds/Backend/config/connection.js
+++
@@ -1,5 +0,0 @@
-module.exports = { 
-    //String has all of the credentials for logining in and populating data within the MovieNerds database
-    mongoURI: 'mongodb+srv://<username>:<password>@movienerds-phttn.mongodb.net/movieNerds'
-
-}
